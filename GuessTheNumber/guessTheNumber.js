5e1360666593f90276b3571b3eef1c06750013de
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-08-07 14:40:23 +0500

    basic structure of js file

---
+++ GuessTheNumber/guessTheNumber.js
@@ -0,0 +1,30 @@
+const randNum = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".lastRsult");
+const lowOrHi = document.querySelector(".lowOrHi");
+const startOver = document.querySelector(".resultParas");
+
+const p = document.createElement("p");
+
+let previousGuess = [];
+let numOfGuesses = 0;
+let playGame = true;
+
+function validateInput(input){
+
+}
+
+function validateInput(guess){
+
+}
+
+function displayInput(input){
+
+}
+
+function showMessage(msg){
+
+}
